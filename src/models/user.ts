6e6dffd555ac885bc8fe66614fc00966ddfe15be
Ifrahim <147778089+IfrahimA@users.noreply.github.com>
2024-10-04 17:49:50 -0400

    added MVC design, included necessary typescript files, and started an skeleton for an express application

---
+++ src/models/user.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const emailSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    }
+})
+
+emailSchema.set("toJSON", {
+    transform: (_document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Email = mongoose.model('Email', emailSchema)
+
+export default Email
+
